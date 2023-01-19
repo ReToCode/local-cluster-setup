ec9943dfbc1c50b208093cca88ad779d39e023a0
Reto Lehmann <retocode@icloud.com>
2023-01-19 13:31:57 +0100

    optimize setup

--- colima_iptables.sh
+++ colima_iptables.sh
@@ -3,7 +3,7 @@
 KIND_IF=$(ip -o link show | awk -F': ' '{print $2}' | grep "br-")
 DST_NET=172.18.0.0/16
 HOST_IF=col0
-SRC_IP=192.168.205.1
+SRC_IP=192.168.106.1 # ip of bridge100 in MacOS
 sudo iptables -t filter -A FORWARD -4 -p tcp -s ${SRC_IP} -d ${DST_NET} -j ACCEPT -i ${HOST_IF} -o ${KIND_IF}
 sudo iptables -L
 
--- mac_route.sh
+++ mac_route.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo route -nv add -net 172.18 192.168.205.3
+COLIMA_IP_ADDR=$(colima ssh -- ip -o -4 a s | grep col0 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+echo "Lima is running on: ${COLIMA_IP_ADDR}"
+sudo route -nv add -net 172.18 ${COLIMA_IP_ADDR}
